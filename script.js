9d7be1d59a3a914bee4b96cd58f159538c7b9fe3
anajuliamazur-creator <ana.julia.mazur@escola.pr.gov.br>
2026-07-06 14:30:25 +0000

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+//cria um constante que seleciona o botão no html
+const botao = document.querySelector("button");
+//cria um evento de click e chama a função botaoClicado
+botao.addEventListener("click",botaoClicado);
+
+//cria a função que fará com que o texto do numero zero some + 1 a cada click.
+function botaoClicado(){
+    let texto = botao.querySelector("span");
+    texto.textContent++;
+}
+
+//         ####legendas:
+//querySelector: seleciona o primeiro elemento que corresponde a um seletor CSS especificando.
+//addEventListener: adiciona um evento de click, permitindo que você execute uma função quando o evento ocorre.
+//textContent: modifica o conteúdo de texto de um elemento HTML.
